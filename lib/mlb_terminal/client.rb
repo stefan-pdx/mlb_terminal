ac51938221a44a20e9a5ddeda03108843bafc921
Stefan Novak <stefan.louis.novak@gmail.com>
2012-09-29 02:02:41 -0400

    update client to list current games

--- lib/mlb_terminal/client.rb
+++ lib/mlb_terminal/client.rb
@@ -1,5 +1,6 @@
 require 'date'
 require 'nokogiri'
+require 'open-uri'
 
 module MLBTerminal
   MLB_BASE_URL = 'http://gd2.mlb.com/components/game/mlb'
@@ -7,7 +8,20 @@ module MLBTerminal
   class Client
     def self.list_games(date = Time.now.to_date)
       doc = Nokogiri::HTML(open "#{base_url_for_date date}/scoreboard_iphone.xml")
-      doc.xpath("//games/game").map(&:attributes)
+      doc.xpath("//games/game").map{|game|
+        {:home_team => {
+            :name   => game["home_team_name"],
+            :wins   => game["home_win"],
+            :losses => game["home_loss"]},
+         :away_team => {
+            :name   => game["home_team_name"],
+            :wins   => game["home_win"],
+            :losses => game["home_loss"]},
+         :score => {
+          :away => (game.xpath("linescore/r").first["away"] rescue nil),
+          :home => (game.xpath("linescore/r").first["home"] rescue nil)},
+         :starts => "#{game["time"]} #{game["time_zone"]}",
+         :status => game.xpath("status").first["ind"]}}
     end
 
     private
